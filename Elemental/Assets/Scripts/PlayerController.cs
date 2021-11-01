efbcdadfe0684b345f9e63d8424a667b594c8fec
Christian Richard <17034@my4county.net>
2021-11-01 14:15:49 -0400

    -New build of Senior Game

---
+++ Elemental/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12;
+    public float sprintSpeed = 20;
+    public float gravity = -29.43f;
+    public float jumpHeight = 4f;
+    public Transform groundCheck;
+    public Transform headCheck;
+    public float groundDistance = 5f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    public bool isSprinting = false;
+    public bool hitHead = false;
+    public GameObject staminaBar;
+    public float sprintCooldown;
+
+    void Update()
+    {
+        checkForMovement();
+        checkForGround();
+        checkForCeiling();
+        checkForJump();
+    }
+
+    public void checkForMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetButton("Sprint") && controller.isGrounded && staminaBar.GetComponent<Slider>().value > 0)
+        {
+            if (move.x < 0 || move.x > 0 || move.y < 0 || move.y > 0 || move.z < 0 || move.z > 0)
+            {
+                isSprinting = true;
+                speed = sprintSpeed;
+                staminaBar.GetComponent<Slider>().value -= Time.deltaTime * 10;
+                sprintCooldown = 5;
+            }
+        }
+        else
+        {
+            isSprinting = false;
+            if (speed > 12)
+            {
+                speed--;
+            }
+
+            if (sprintCooldown > 0)
+            {
+                sprintCooldown -= Time.deltaTime;
+            }
+        }
+
+        if (staminaBar.GetComponent<Slider>().value < 100 && sprintCooldown <= 0)
+        {
+            staminaBar.GetComponent<Slider>().value += Time.deltaTime * 5;
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    public void checkForGround()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    public void checkForJump()
+    {
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    //checkForCeiling - uses the collsionFlags on the controller to check if a collision occured at the top of the player.
+
+    public void checkForCeiling()
+    {
+        if ((controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            velocity.y = 0f;
+        }
+    }
+}
---
+++ Elemental/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+    public GameObject healthText;
+    public GameObject healthImage;
+    public Texture fullHealthIMG;
+    public Texture halfHealthIMG;
+    public Texture quarterHealthIMG;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        setHealthBar();
+    }
+
+    public void setHealthBar()
+    {
+        healthText.GetComponent<TextMeshProUGUI>().text = "" + currentHealth;
+        setHealthIMG(currentHealth);
+    }
+
+    public void setHealthIMG(float healthValue)
+    {
+        float halfHealth = maxHealth/2;
+        float quarterHealth = maxHealth/4;
+
+        if(healthValue > halfHealth)
+        {
+            healthImage.GetComponent<RawImage>().texture = fullHealthIMG;
+        }
+        else if(healthValue <= halfHealth && healthValue > quarterHealth)
+        {
+            healthImage.GetComponent<RawImage>().texture = halfHealthIMG;
+        }
+        else
+        {
+            healthImage.GetComponent<RawImage>().texture = quarterHealthIMG;
+        }
+    }
+
+    public void loseHealth(float damageValue)
+    {
+        currentHealth -= damageValue;
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        setHealthBar();
+    }
+
+    public void gainHealth(float healthValue)
+    {
+        currentHealth += healthValue;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        setHealthBar();
+    }
+}
