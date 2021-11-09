e5f3e4ccb00b87db1a59f1ba3f390158ba280fdb
Christian Richard <17034@my4county.net>
2021-11-09 12:27:56 -0500

    -message added for GameManager

--- Elemental/Assets/Scripts/GameManager.cs
+++ Elemental/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class GameManager: MonoBehaviour
 {
 
+    // takes in a string containing the name of a scene and loads it through SceneManager.
     public void Teleport(string location)
     {
         SceneManager.LoadScene(location);
