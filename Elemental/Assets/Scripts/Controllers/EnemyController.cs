1772ede8d9a18fcf4be65a3980e6584460383fde
Christian Richard <17034@my4county.net>
2021-11-15 14:51:57 -0500

    -changes to player-enemy interaction

---
+++ Elemental/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform target;
+    public float lookRadius = 10f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+
+    }
+}
