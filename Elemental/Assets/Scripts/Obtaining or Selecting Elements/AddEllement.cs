47781aefd7eba246944da9f369958649f57eb540
crichard17034 <17034@my4county.net>
2021-12-21 12:51:00 -0500

    -fix for merge conflict

---
+++ Elemental/Assets/Scripts/Obtaining or Selecting Elements/AddEllement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEllement : MonoBehaviour
+{
+
+    public bool isFireGem = false;
+    public bool isWaterGem = false;
+    public bool isWindGem = false;
+
+    public bool hasFireElem = false;
+    public bool hasWaterElem = false;
+    public bool hasWindElem = false;
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+
+            if (isFireGem == true) 
+            { 
+                ChangeEllement.setElem = "Fire";
+                Debug.Log("The Player Picked up a Fire Gem");
+            }
+            if (isWaterGem == true) 
+            {     
+                ChangeEllement.setElem = "Water";
+                Debug.Log("The Player Picked up a Water Gem");
+
+            }
+            if (isWindGem == true) 
+            { 
+                ChangeEllement.setElem = "Wind";
+                Debug.Log("The Player Picked up a Wind Gem");
+
+
+            }
+
+        }
+    }
+}
